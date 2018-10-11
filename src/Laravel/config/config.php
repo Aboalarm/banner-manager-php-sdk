96d2e54603119b19a8421979ce90f5c548acaf0f
Evis Bregu <evis.bregu@gmail.com>
2018-10-11 16:50:41 +0200

    Prefixing env vars with BANNER_SDK_API

--- src/Laravel/config/config.php
+++ src/Laravel/config/config.php
@@ -1,9 +1,9 @@
 <?php
 return [
     // Base API uri
-    'base_uri' => env('API_BASE_URI'),
+    'base_uri' => env('BANNER_SDK_API_BASE_URI'),
     // The API user.
-    'username' => env('API_USER'),
+    'username' => env('BANNER_SDK_API_USERNAME'),
     // The API password.
-    'password' => env('API_PASSWORD'),
+    'password' => env('BANNER_SDK_API_PASSWORD'),
 ];
