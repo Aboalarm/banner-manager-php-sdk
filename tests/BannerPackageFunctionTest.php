7161a2335f3a6754003a28136aa64a23f41d1d51
Evis Bregu <evis.bregu@gmail.com>
2018-10-12 13:25:47 +0200

    Added render method to SDK client

--- tests/BannerPackageFunctionTest.php
+++ tests/BannerPackageFunctionTest.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace aboalarm\BannerManagerSdk\Test;
 
 use BannerSDK;
@@ -12,5 +13,12 @@ class BannerPackageFunctionTest extends TestCase
     public function testGetBanners()
     {
         $this->assertNotEmpty(BannerSDK::getBanners());
+    }
+
+    public function testRenderBanner()
+    {
+        $this->assertNotEmpty(
+            BannerSDK::render('adr_1235_top_horizotal', 'session', 'test_device')
+        );
     }
 }
