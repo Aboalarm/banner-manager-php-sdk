835db7e529b7b32bfffbd37123628130786bcc3b
Evis Bregu <evis.bregu@gmail.com>
2018-11-12 10:04:28 +0100

    Added BannerManagerException to be thrown when problems arise in client

---
+++ src/Exception/BannerManagerException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by Evis Bregu <evis.bregu@gmail.com>.
+ * Date: 11/12/18
+ * Time: 9:32 AM
+ */
+
+namespace aboalarm\BannerManagerSdk\Exception;
+
+
+use Exception;
+
+class BannerManagerException extends Exception
+{
+
+}
