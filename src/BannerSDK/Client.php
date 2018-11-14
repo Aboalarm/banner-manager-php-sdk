14e2b7b807590fe085f5bc6fae7f947c0dd8f2e1
Evis Bregu <evis.bregu@gmail.com>
2018-11-14 14:19:32 +0100

    Added methods to add/remove banner from/to campaign

--- src/BannerSDK/Client.php
+++ src/BannerSDK/Client.php
@@ -138,6 +138,41 @@ class Client
     }
 
     /**
+     * Post multiple campaigns to be assigned to a banner
+     *
+     * @param string $identifier          Banner identifier
+     * @param array  $campaignIdentifiers Array with campaign identifiers to add
+     *
+     * @return array
+     * @throws BannerManagerException
+     * @throws GuzzleException
+     */
+    public function postBannerCampaigns(string $identifier, array $campaignIdentifiers)
+    {
+        $uri = '/api/banners/'.$identifier.'/campaigns';
+        $formParams = ['campaigns' => $campaignIdentifiers];
+
+        return $this->doPostRequest($uri, $formParams);
+    }
+
+    /**
+     * Remove a campaign from banner
+     *
+     * @param string $bannerIdentifier
+     * @param string $campaignIdentifier
+     *
+     * @return bool
+     * @throws BannerManagerException
+     * @throws GuzzleException
+     */
+    public function removeCampaignFromBanner(string $bannerIdentifier, string $campaignIdentifier)
+    {
+        $uri = '/api/banners/'.$bannerIdentifier.'/campaigns/'.$campaignIdentifier;
+
+        return $this->doDeleteRequest($uri);
+    }
+
+    /**
      * Get all campaigns.
      *
      * @return PaginatedCollection Campaign collection.
@@ -216,6 +251,41 @@ class Client
     public function deleteCampaign(string $identifier)
     {
         $uri = '/api/campaigns/'.$identifier;
+
+        return $this->doDeleteRequest($uri);
+    }
+
+    /**
+     * Post multiple banners to be assigned to a campaign
+     *
+     * @param string $identifier        Campaign identifier
+     * @param array  $bannerIdentifiers Array with banner identifiers to add
+     *
+     * @return array
+     * @throws BannerManagerException
+     * @throws GuzzleException
+     */
+    public function postCampaignBanners(string $identifier, array $bannerIdentifiers)
+    {
+        $uri = '/api/campaigns/'.$identifier.'/banners';
+        $formParams = ['banners' => $bannerIdentifiers];
+
+        return $this->doPostRequest($uri, $formParams);
+    }
+
+    /**
+     * Remove a banner from campaign
+     *
+     * @param string $campaignIdentifier
+     * @param string $bannerIdentifier
+     *
+     * @return bool
+     * @throws BannerManagerException
+     * @throws GuzzleException
+     */
+    public function removeBannerFromCampaign(string $campaignIdentifier, string $bannerIdentifier)
+    {
+        $uri = '/api/campaigns/'.$campaignIdentifier.'/banners/'.$bannerIdentifier;
 
         return $this->doDeleteRequest($uri);
     }
@@ -522,9 +592,10 @@ class Client
      * @throws GuzzleException
      */
     private function doPostRequest(string $uri, array $formParams)
-    {        $response = $this->doRequest('POST', $uri, null, $formParams);
+    {
+        $response = $this->doRequest('POST', $uri, null, $formParams);
 
-        if ($response->getStatusCode() !== 201) {
+        if ($response->getStatusCode() !== 201 && $response->getStatusCode() !== 200) {
             throw new BannerManagerException(
                 $response->getReasonPhrase(),
                 $response->getStatusCode()
